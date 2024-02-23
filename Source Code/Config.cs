4bf593f8c41f98119059e49c9fce526b77612751
FrizaSCPSL <161040188+FrizaSCPSL@users.noreply.github.com>
2024-02-23 20:45:47 +0300

    Add files via upload

---
+++ Source Code/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+
+namespace Narkomania
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+        
+        public int healthPercent { get; set; } = 200;
+    }
+}
